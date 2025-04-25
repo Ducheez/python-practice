d69bce2bffb83d45b7451fcf4dbc15e9e2d863ba
Ducheez <njokivanessa2005@gmail.com>
2025-04-25 18:49:13 +0300

    Number one

---
+++ sum_list.py
@@ -0,0 +1,8 @@
+# I created an array called x which had 4 numbers
+x = [5, 8, 20, 7]
+
+# Created a variable called ans which finds the sum of elements in the array
+ans = sum(x)
+
+# Used a print function to display the sum of the elements
+print("Total sum:", ans)
